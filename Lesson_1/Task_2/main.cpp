ec02903191cccc2ff91d357dd979742a8649e2e7
dianxqwidi <dianaradomska4@gmail.com>
2025-05-10 20:46:54 +0300

    modified Task_2

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -4,43 +4,43 @@
 using namespace std;
 
 int main() {
-    double a, b;
+    double number_one, number_two;
     char operation;
 
     cout << "Welcome! It's Calculator:)" << endl;
     cout << "Enter first number: " << endl;
-    cin >> a;
+    cin >> number_one;
     cout << "Enter operation (+, -, *, /, ^, r): " << endl;
     cin >> operation;
 
     if (operation != 'r') { // If the user selects 'r', the calculator will compute the square root of the first number only.
         cout << "Enter second number: " << endl;
-        cin >> b;
+        cin >> number_two;
     }
 
     switch (operation) {
     case '+':
-        cout << a << " + " << b << " = " << a + b << endl;
+        cout << number_one << " + " << number_two << " = " << number_one + number_two << endl;
         break;
     case '-':
-        cout << a << " - " << b << " = " << a - b << endl;
+        cout << number_one << " - " << number_two << " = " << number_one - number_two << endl;
         break;
     case '*':
-        cout << a << " * " << b << " = " << a * b << endl;
+        cout << number_one << " * " << number_two << " = " << number_one * number_two << endl;
         break;
     case '/':
-        if (b != 0) {
-            cout << a << " / " << b << " = " << a / b << endl;
+        if (number_two != 0) {
+            cout << number_one << " / " << number_two << " = " << number_one / number_two << endl;
         } else {
             cout << "Error: Division by zero!" << endl;
         }
         break;
     case '^':
-        cout << a << " ^ " << b << " = " << pow(a, b) << endl;
+        cout << number_one << " ^ " << number_two << " = " << pow(number_one, number_two) << endl;
         break;
     case 'r':
-        if (a >= 0) {
-            cout << "Square root of " << a << " = " << sqrt(a) << endl;
+        if (number_one >= 0) {
+            cout << "Square root of " << number_one << " = " << sqrt(number_one) << endl;
         } else {
             cout << "Error: Square root of negative number!" << endl;
         }
