26672aa8e4cb6f25169b0c711484c0a0607b53dd
dianxqwidi <dianaradomska4@gmail.com>
2025-05-10 22:36:23 +0300

    modified Task_2

--- Lesson_2/Task_2/main.cpp
+++ Lesson_2/Task_2/main.cpp
@@ -2,17 +2,48 @@
 
 using namespace std;
 
-int main() {
-    int number[5];
-    for (int n = 0; n < 5; n++) {
-        cout << "Enter number: " ;
-        cin >> number[n];
+void enterNumbers(int userNumbers[], int arraySize) {
+    for (int index = 0; index < arraySize; index++) {
+        cout << "Enter number " << index + 1 << ": ";
+        cin >> userNumbers[index];
+
+        if (userNumbers[index] < 0) {
+            cout << "(Please enter a number greater than or equal to 0)\n";
+            index--;
+        }
     }
-    for (int n = 0; n < 5; n++) {
-        for (int p = 0; p < number[n]; p++) {
+}
+
+void drawStars(const int userNumbers[], int arraySize) {
+    int maxValue = 0;
+
+    for (int index = 0; index < arraySize; index++) {
+        if (userNumbers[index] > maxValue) {
+            maxValue = userNumbers[index];
+        }
+    }
+
+    for (int index = 0; index < arraySize; index++) {
+        int spaces = maxValue - userNumbers[index];
+        for (int starCounter = 0; starCounter < spaces; starCounter++) {
+            cout << " ";
+        }
+        for (int starCounter = 0; starCounter < userNumbers[index]; starCounter++) {
             cout << "*";
         }
         cout << endl;
     }
+}
+
+int main() {
+    const int numberInputs = 5;
+    int userNumbers[numberInputs];
+
+    cout << "Enter 5 non-negative numbers\n";
+    enterNumbers(userNumbers, numberInputs);
+
+    cout << "\nDrawing stars:\n";
+    drawStars(userNumbers, numberInputs);
+
     return 0;
 }
